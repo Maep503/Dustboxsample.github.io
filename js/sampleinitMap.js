cff70eb90ebb019c4c20e09772c9e113c71670b1
Yousuke Maeda <s1F101701755@iniad.org>
2019-12-04 14:48:28 +0900

    pinが出なくなる問題を修正

--- js/sampleinitMap.js
+++ js/sampleinitMap.js
@@ -16,13 +16,13 @@ function initMap() {
    });
 }
 
-function pin() {
+/*function pin() {
   //navigator.geolocation.getCurrentPosition(pin2);
   var date = new Date(position.timestamp);
-  var alerttext = "取得時刻:" + date.toLocaleString() + "にゴミ箱のピンをうちました"+"\n";
+  var alerttext = "取得時刻:" + date.toLocaleString() +"\n";
   alert(alerttext);
   
-  /*map = new google.maps.Map(document.getElementById('maps'), { // #sampleに地図を埋め込む
+  map = new google.maps.Map(document.getElementById('maps'), { // #sampleに地図を埋め込む
     center: center, // 地図の中心を指定
     zoom: 19 // 地図のズームを指定
   });
@@ -59,3 +59,25 @@ function pin() {
   alert(alerttext);
 
 }*/
+
+function pin() {
+  navigator.geolocation.getCurrentPosition(test2);
+}
+
+function test2(position) {
+
+  var geo_text = "緯度:" + position.coords.latitude + "\n";
+  geo_text += "経度:" + position.coords.longitude + "\n";
+  geo_text += "高度:" + position.coords.altitude + "\n";
+  geo_text += "位置精度:" + position.coords.accuracy + "\n";
+  geo_text += "高度精度:" + position.coords.altitudeAccuracy  + "\n";
+  geo_text += "移動方向:" + position.coords.heading + "\n";
+  geo_text += "速度:" + position.coords.speed + "\n";
+
+  var date = new Date(position.timestamp);
+
+  geo_text += "取得時刻:" + date.toLocaleString() + "\n";
+
+  alert(geo_text);
+
+  }
